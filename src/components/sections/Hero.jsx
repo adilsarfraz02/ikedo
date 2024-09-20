120268e1bd33a9ccf51607429336ec7fc7b71394
Muhammad ADil <129647437+Azhan52@users.noreply.github.com>
2024-09-20 03:38:34 -0700

    Update image issues

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -57,7 +57,7 @@ export default function HeroSection() {
           </p>
           <div className='grid grid-cols-2 gap-4'>
             <Image
-              src='/Pending_acc.png'
+              src='https://raw.githubusercontent.com/adilsarfraz02/ikedo/refs/heads/master/public/pending_acc.png'
               alt='hero template'
               width={500}
               height={500}
@@ -71,7 +71,7 @@ export default function HeroSection() {
               className='rounded-lg bg-zinc-800 object-contain h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
             />
             <Image
-              src='/pricing.png'
+              src='https://raw.githubusercontent.com/adilsarfraz02/ikedo/refs/heads/master/public/Pricing.png'
               alt='feature template'
               width={500}
               height={500}
